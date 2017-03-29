1d7c19f8367c4d557922e4b75fa7620fc3011219
Rob Gloudemans <Rob_Gloudemans@hotmail.com>
2017-03-29 21:37:23 +0200

    Added section for the home slider and sync the index.php file with front-page.php

---
+++ section-home.php
@@ -0,0 +1,232 @@
+<!-- Section: home -->
+<section id="home" class="divider no-bg">
+
+    <div class="container-fluid p-0">
+        <div id="revolution-slider-fullscreen_wrapper" class="rev_slider_wrapper fullscreen-container"
+             data-alias="concept1" style="background-color:#000000;padding:0px;">
+            <!-- START REVOLUTION SLIDER 5.1.1RC fullscreen mode -->
+            <div id="revolution-slider-fullscreen" class="rev_slider fullscreenbanner"
+                 style="display:none; height: 137px;" data-version="5.1.1RC">
+
+                <ul>
+                    <!-- SLIDE 1 -->
+                    <li data-index="rs-1" data-transition="random" data-slotamount="7" data-easein="default"
+                        data-easeout="default" data-masterspeed="1000" src="http://themes.sumerianparadise.com/demo/html/nailsbeauty/images/slider_1.jpg"
+                        data-rotate="0" data-fstransition="fade" data-fsmasterspeed="1500" data-fsslotamount="7"
+                        data-saveperformance="off" data-title="Intro" data-description="">
+                        <!-- MAIN IMAGE -->
+                        <img src="http://themes.sumerianparadise.com/demo/html/nailsbeauty/images/slider_1.jpg" data-bgparallax="10" alt=""
+                             data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"
+                             class="rev-slidebg" data-no-retina>
+                        <!-- LAYERS -->
+
+                        <!-- LAYER NR. 1 -->
+                        <div class="tp-caption tp-shape tp-shapewrapper tp-resizeme" id="rs-1-layer-1"
+                             data-x="['middle']" data-hoffset="['0']" data-y="['middle']" data-voffset="['30']"
+                             data-width="2000" data-height="2000" data-whitespace="nowrap"
+                             data-transform_idle="o:1;"
+                             data-transform_in="y:[0%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[0%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:100px;y:[0%];s:inherit;e:inherit;"
+                             data-mask_out="x:0;y:0;s:inherit;e:inherit;" data-start="500"
+                             data-responsive_offset="on"
+                             style="z-index: 5;background-color:rgba(0, 0, 0, 0.70);border-color:rgba(0, 0, 0, 0);"></div>
+
+                        <!-- LAYER NR. 2 -->
+                        <div class="tp-caption tp-resizeme text-white font-Playfair-Display font-weight-bold font-italic"
+                             id="rs-1-layer-2" data-x="['middle']" data-hoffset="['0']" data-y="['200']"
+                             data-voffset="['-45']" data-fontsize="['40']" data-lineheight="['120']"
+                             data-width="none"
+                             data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;"
+                             data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                             data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1000"
+                             data-splitin="none" data-splitout="none" data-responsive_offset="on"
+                             style="z-index: 7; white-space: nowrap;">your nails are
+                        </div>
+
+
+                        <!-- LAYER NR. 3 -->
+                        <div class="tp-caption tp-resizeme text-white font-Playfair-Display font-weight-bold"
+                             id="rs-1-layer-3" data-x="['middle']" data-hoffset="['0']" data-y="['300']"
+                             data-voffset="['-45']" data-fontsize="['90']" data-lineheight="['120']"
+                             data-width="none"
+                             data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;"
+                             data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                             data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1000"
+                             data-splitin="none" data-splitout="none" data-responsive_offset="on"
+                             style="z-index: 7; white-space: nowrap;">THE REFLECTION
+                        </div>
+
+                        <!-- LAYER NR. 4 -->
+                        <div class="tp-caption tp-resizeme text-white font-Playfair-Display" id="rs-1-layer-4"
+                             data-x="['middle']" data-hoffset="['0']" data-y="['400']" data-voffset="['-45']"
+                             data-fontsize="['60']" data-lineheight="['120']" data-width="none"
+                             data-height="none"
+                             data-whitespace="nowrap" data-transform_idle="o:1;"
+                             data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                             data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1000"
+                             data-splitin="none" data-splitout="none" data-responsive_offset="on"
+                             style="z-index: 7; white-space: nowrap;">of yourself
+                        </div>
+                    </li>
+
+                    <!-- SLIDE 2 -->
+                    <li data-index="rs-2" data-transition="random" data-slotamount="7" data-easein="default"
+                        data-easeout="default" data-masterspeed="1000" src="http://themes.sumerianparadise.com/demo/html/nailsbeauty/images/slider_2.jpg"
+                        data-rotate="0" data-fstransition="fade" data-fsmasterspeed="1500" data-fsslotamount="7"
+                        data-saveperformance="off" data-title="Intro" data-description="">
+                        <!-- MAIN IMAGE -->
+                        <img src="http://themes.sumerianparadise.com/demo/html/nailsbeauty/images/slider_2.jpg" data-bgparallax="10" alt=""
+                             data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"
+                             class="rev-slidebg" data-no-retina>
+                        <!-- LAYERS -->
+
+                        <!-- LAYER NR. 1 -->
+                        <div class="tp-caption tp-shape tp-shapewrapper tp-resizeme" id="rs-2-layer-1"
+                             data-x="['middle']" data-hoffset="['15']" data-y="['middle']" data-voffset="['30']"
+                             data-width="2000" data-height="2000" data-whitespace="nowrap"
+                             data-transform_idle="o:1;"
+                             data-transform_in="y:[0%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[0%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:100px;y:[0%];s:inherit;e:inherit;"
+                             data-mask_out="x:0;y:0;s:inherit;e:inherit;" data-start="500"
+                             data-responsive_offset="on"
+                             style="z-index: 5;background-color:rgba(0, 0, 0, 0.70);border-color:rgba(0, 0, 0, 0);"></div>
+
+                        <!-- LAYER NR. 2 -->
+                        <div class="tp-caption tp-resizeme text-white font-Playfair-Display font-weight-bold font-italic"
+                             id="rs-2-layer-2" data-x="['200']" data-hoffset="['60']" data-y="['200']"
+                             data-voffset="['-45']" data-fontsize="['40']" data-lineheight="['110']"
+                             data-width="none"
+                             data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;"
+                             data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                             data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1000"
+                             data-splitin="none" data-splitout="none" data-responsive_offset="on"
+                             style="z-index: 7; white-space: nowrap;">nails take
+                        </div>
+
+                        <!-- LAYER NR. 3 -->
+                        <div class="tp-caption tp-resizeme text-white font-Playfair-Display font-weight-bold"
+                             id="rs-2-layer-3" data-x="['200']" data-hoffset="['60']" data-y="['300']"
+                             data-voffset="['-45']" data-fontsize="['90']" data-lineheight="['110']"
+                             data-width="none" data-height="none"
+                             data-whitespace="nowrap" data-transform_idle="o:1;"
+                             data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                             data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1000"
+                             data-splitin="none" data-splitout="none" data-responsive_offset="on"
+                             style="z-index: 7; white-space: nowrap;">ANY OLD OUTFIT
+                        </div>
+
+                        <!-- LAYER NR. 4 -->
+                        <div class="tp-caption tp-resizeme text-white font-Playfair-Display" id="rs-2-layer-4"
+                             data-x="['200']" data-hoffset="['60']" data-y="['400']" data-voffset="['-45']"
+                             data-fontsize="['60']" data-lineheight="['110']" data-width="none"
+                             data-height="none"
+                             data-whitespace="nowrap" data-transform_idle="o:1;"
+                             data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                             data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1000"
+                             data-splitin="none" data-splitout="none" data-responsive_offset="on"
+                             style="z-index: 7; white-space: nowrap;">and make it new
+                        </div>
+                    </li>
+
+                    <!-- SLIDE 3 -->
+                    <li data-index="rs-3" data-transition="random" data-slotamount="7" data-easein="default"
+                        data-easeout="default" data-masterspeed="1000" src="http://themes.sumerianparadise.com/demo/html/nailsbeauty/images/slider_3.jpg"
+                        data-rotate="0" data-fstransition="fade" data-fsmasterspeed="1500" data-fsslotamount="7"
+                        data-saveperformance="off" data-title="Intro" data-description="">
+
+                        <!-- MAIN IMAGE -->
+                        <img src="http://themes.sumerianparadise.com/demo/html/nailsbeauty/images/slider_3.jpg" data-bgparallax="10" alt=""
+                             data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"
+                             class="rev-slidebg" data-no-retina>
+                        <!-- LAYERS -->
+
+                        <!-- LAYER NR. 1 -->
+                        <div class="tp-caption tp-shape tp-shapewrapper tp-resizeme" id="rs-3-layer-1"
+                             data-x="['middle']" data-hoffset="['15']" data-y="['middle']" data-voffset="['30']"
+                             data-width="2000" data-height="2000" data-whitespace="nowrap"
+                             data-transform_idle="o:1;"
+                             data-transform_in="y:[0%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[0%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:100px;y:[0%];s:inherit;e:inherit;"
+                             data-mask_out="x:0;y:0;s:inherit;e:inherit;" data-start="500"
+                             data-responsive_offset="on"
+                             style="z-index: 5;background-color:rgba(0, 0, 0, 0.70);border-color:rgba(0, 0, 0, 0);"></div>
+
+                        <!-- LAYER NR. 2 -->
+                        <div class="tp-caption tp-resizeme text-white font-Playfair-Display font-weight-bold"
+                             id="rs-3-layer-2" data-x="['middle']" data-hoffset="['0']" data-y="['200']"
+                             data-voffset="['-45']" data-fontsize="['90']" data-lineheight="['120']"
+                             data-width="none"
+                             data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;"
+                             data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                             data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1000"
+                             data-splitin="none" data-splitout="none" data-responsive_offset="on"
+                             style="z-index: 7; white-space: nowrap;">TRUE BEAUTY
+                        </div>
+
+                        <!-- LAYER NR. 3 -->
+                        <div class="tp-caption tp-resizeme text-white font-Playfair-Display" id="rs-3-layer-3"
+                             data-x="['middle']" data-hoffset="['0']" data-y="['300']" data-voffset="['-45']"
+                             data-fontsize="['60']" data-lineheight="['120']" data-width="none"
+                             data-height="none"
+                             data-whitespace="nowrap" data-transform_idle="o:1;"
+                             data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                             data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1000"
+                             data-splitin="none" data-splitout="none" data-responsive_offset="on"
+                             style="z-index: 7; white-space: nowrap;">comes from within
+                        </div>
+
+
+                        <!-- LAYER NR. 4 -->
+                        <div class="tp-caption tp-shape tp-shapewrapper tp-resizeme" id="rs-3-layer-4"
+                             data-x="['365']" data-hoffset="['15']" data-y="['460']" data-voffset="['30']"
+                             data-width="430" data-height="80" data-whitespace="nowrap"
+                             data-transform_idle="o:1;"
+                             data-transform_in="y:[0%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[0%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:100px;y:[0%];s:inherit;e:inherit;"
+                             data-mask_out="x:0;y:0;s:inherit;e:inherit;" data-start="500"
+                             data-responsive_offset="on"
+                             style="z-index: 5; background-color:rgba(1, 203, 217, 0.70);border-color:rgba(0, 0, 0, 0);"></div>
+
+                        <!-- LAYER NR. 5 -->
+                        <div class="tp-caption tp-resizeme font-weight-bold" id="rs-3-layer-5"
+                             data-x="['center']" data-hoffset="['0']" data-y="['450']" data-voffset="['120']"
+                             data-fontsize="['25']" data-lineheight="['50']" data-width="none"
+                             data-height="none" data-whitespace="nowrap"
+                             data-transform_idle="o:1;"
+                             data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
+                             data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
+                             data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
+                             data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;" data-start="1400"
+                             data-splitin="none" data-splitout="none" data-responsive_offset="on"
+                             style="z-index: 5; white-space: nowrap; letter-spacing:1px;">
+                            <a class="btn btn-colored btn-text-white-hover-blue" href="page-booknow.html">SCHEDULE
+                                AN APPOINTMENT</a></div>
+                    </li>
+                </ul>
+
+                <div class="tp-bannertimer tp-top"
+                     style="height: 7px; background-color: rgba(0, 0, 0, 0.1);"></div>
+            </div>
+        </div>
+    </div>
+</section>
