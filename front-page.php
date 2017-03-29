6e8bdf8cd3a7a6ad08376324c2bb68aa40c3155e
Rob Gloudemans <Rob_Gloudemans@hotmail.com>
2017-03-29 20:54:30 +0200

    Working on about section

--- front-page.php
+++ front-page.php
@@ -235,49 +235,7 @@
         </div>
     </section>
 
-    <!-- Section: About US -->
-    <section id="features" class="overflow-visible">
-        <div class="container pt-0">
-            <div class="section-content pt-0">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="features-health p-30 pb-10 pr-sm-15 pl-xs-15" data-bg-color="#FFF">
-                            <h2 class="text-center font-italic"><span>About Our Salon</span></h2>
-                            <div class="subtitle-divider-2 text-center divider-about-us"></div>
-                            <div class="icon-box left media mt-30">
-                                <?php if (have_posts()) : while(have_posts()) : the_post();?>
-                                    <div class="col-md-6"><img class="img-fullwidth" src="http://themes.sumerianparadise.com/demo/html/nailsbeauty/images/about.jpg"
-                                                               alt=""></div>
-                                    <div class="col-md-6 mt-sm-30">
-                                        <p class="text-light-gray">
-                                            Lorem ipsum dolor sit amet, ludus maluisset his cu, ut prodesset posidonium
-                                            nam, quo facilisis democritum eloquentiam at. Pri ad ipsum assueverit, cu
-                                            sea possim concludaturque. No eos aliquid phaedrum, ex viderer salutandi
-                                            mea. In has prima assum voluptatibus.
-                                        </p>
-                                        <p class="text-light-gray mt-20">
-                                            Vel eros option mediocrem no. In quidam dolorem mei, in ornatus sententiae
-                                            percipitur has. Cu vim amet singulis, mei saepe ceteros partiendo ex. Eam
-                                            tation eirmod ei, utamur delectus vis id. Iudico maiestatis mea ut, ne
-                                            admodum consulatu mea, suas tamquam
-                                            vis at.
-                                        </p>
-                                        <p class="text-light-gray mt-20">
-                                            Mea graece labitur ad, eripuit explicari concludaturque mel te. Falli
-                                            patrioque splendide te pri, vim ubique virtute ponderum no.
-                                        </p>
-                                        <a class="btn btn-colored btn-white-border-black btn-light-blue-hover hvr-shutter-out-horizontal mt-30"
-                                           href="javascript:void(0)" data-border="1px solid #CDCDCD"
-                                           data-mouseover-color="#fff" data-mouseout-color="#000">READ MORE</a>
-                                    </div>
-                                <?php endwhile; endif;?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php get_template_part('section', 'about');?>
 
     <?php get_template_part('section', 'services');?>
 
--- settings.php
+++ settings.php
@@ -83,6 +83,14 @@ add_action('admin_init', function () {
     register_setting('section', 'homepage_google_maps');
     register_setting('section', 'homepage_mailchimp_form');
 
+    add_settings_field('section_about_title', 'About section title', function () {
+        ?><input type="text" name="section_about_title" id="section_about_title" class="regular-text" value="<?php echo esc_attr(get_option('section_about_title'));?>" /> <?php
+    }, 'theme-options', 'section');
+
+    add_settings_field('section_about_subtitle', 'About section subtitle', function () {
+        ?><input type="text" name="section_about_subtitle" id="section_about_subtitle" class="regular-text" value="<?php echo esc_attr(get_option('section_about_subtitle'));?>" /> <?php
+    }, 'theme-options', 'section');
+
     add_settings_field('section_services_title', 'Services section title', function () {
         ?><input type="text" name="section_services_title" id="section_services_title" class="regular-text" value="<?php echo esc_attr(get_option('section_services_title'));?>" /> <?php
     }, 'theme-options', 'section');
@@ -107,6 +115,8 @@ add_action('admin_init', function () {
         ?><input type="text" name="section_contact_subtitle" id="section_contact_subtitle" class="regular-text" value="<?php echo esc_attr(get_option('section_contact_subtitle'));?>" /> <?php
     }, 'theme-options', 'section');
 
+    register_setting('section', 'section_about_title');
+    register_setting('section', 'section_about_subtitle');
     register_setting('section', 'section_services_title');
     register_setting('section', 'section_services_subtitle');
     register_setting('section', 'section_portfolio_title');
